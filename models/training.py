0aa4349983ccaadfe5b68c4cfb5e97d5efedf866
Dalton Fung <fungdc@bc.edu>
2020-07-20 14:18:58 +0800

    Changed training.py to accommodate multiclass

--- models/training.py
+++ models/training.py
@@ -1,4 +1,4 @@
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import SGDRegression
 from preprocessing import *
 import pickle
 
@@ -8,14 +8,12 @@ train_labels = convert_idx_to_array('../data/train_labels')
 test_img = convert_idx_to_array('../data/test_images')
 test_labels = convert_idx_to_array('../data/test_labels')
 
-train_labels = convert_labels(5,train_labels)
-test_labels = convert_labels(5,test_labels)
 
 train_img = flatten_img(train_img)
 test_img = flatten_img(test_img)
 
-logreg = LogisticRegression()
-logreg.fit(train_img, train_labels)
+sgd_clf = SGDRegression(random_seed = 10)
+sgd_clf.fit(train_img, train_labels)
 
 filename = 'logistic_regression_model.sav'
 pickle.dump(logreg, open(filename, 'wb'))
